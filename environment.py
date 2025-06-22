6edcf2201e5c7eae0b4890a21223db0fcd3ea96c
Stefan <stefan-horst@gmx.de>
2025-06-22 17:49:06 +0200

    update abstract anvironment

--- environment.py
+++ environment.py
@@ -1,17 +1,31 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class Environment(ABC):
     
-    def __init__(self, seed=None):
-        self._seed = seed
-        self.info = {"action_space_size": 0}
+    def __init__(self, 
+                 observation_space_size: int, 
+                 observation_space_spec: Any, 
+                 action_space_size: int, 
+                 action_space_spec: Any,
+                 reward_spec: Any):
+        self.observation_space_size = observation_space_size
+        self.observation_space_spec = observation_space_spec
+        self.action_space_size = action_space_size
+        self.action_space_spec = action_space_spec
+        self.reward_spec = reward_spec
+        self.terminated = False
+        self.seed = None
     
     @abstractmethod
-    def initialize(self):
+    def initialize(self, seed: int | None = None) -> tuple[Any, dict[str, Any]]:
         pass
     
     @abstractmethod
-    def act(self, action):
+    def reset(self) -> tuple[Any, dict[str, Any]]:
         pass
     
+    @abstractmethod
+    def act(self, action: Any) -> tuple[Any, float, bool, bool, dict[str, Any]]:
+        pass
