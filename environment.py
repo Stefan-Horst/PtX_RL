2b7189697ac96f47a3be79294b9b28b2570c2005
Stefan <stefan-horst@gmx.de>
2025-06-26 05:27:24 +0200

    add ptx environment skeleton

--- environment.py
+++ environment.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
+from copy import copy
 from typing import Any
 import gymnasium as gym
+
+from ptx.framework import ParameterObject
 
 
 class Environment(ABC):
@@ -66,3 +69,51 @@ class GymEnvironment(Environment):
         observation, reward, terminated, truncated, info = self.env.step(action)
         self.terminated = terminated or truncated
         return observation, reward, terminated, truncated, info
+
+
+# only include attributes which are variable during the simulation
+COMMODITY_ATTRIBUTES = []
+CONVERSION_ATTRIBUTES = []
+STORAGE_ATTRIBUTES = []
+GENERATOR_ATTRIBUTES = []
+
+class PtxEnvironment(Environment):
+    
+    def __init__(self, ptx_system: ParameterObject, 
+                 commodity_attributes=COMMODITY_ATTRIBUTES, conversion_attributes=CONVERSION_ATTRIBUTES, 
+                 storage_attributes=STORAGE_ATTRIBUTES, generator_attributes=GENERATOR_ATTRIBUTES):
+        """Create environment with PtX sytem to use and optionally specify relevant attributes for the agent"""
+        self.commodity_attributes = commodity_attributes
+        self.conversion_attributes = conversion_attributes
+        self.storage_attributes = storage_attributes
+        self.generator_attributes = generator_attributes
+        self._original_ptx_system = ptx_system
+        super().__init__(None, None, None, None, None)
+        
+    def initialize(self, seed=None):
+        self.seed = seed
+        self.ptx_system = copy(self._original_ptx_system)
+        return None, None
+    
+    def reset(self):
+        self.ptx_system = copy(self._original_ptx_system)
+        self.terminated = False
+        return None, None
+    
+    def act(self, action):
+        self._apply_action(action)
+        
+        reward = self._calculate_reward()
+        return None, reward, False, False, None
+    
+    def _get_observation_space(self):
+        return
+    
+    def _get_action_space(self):
+        return
+    
+    def _apply_action(self, action):
+        return
+    
+    def _calculate_reward(self):
+        return
