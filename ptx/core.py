6d3291a02c9ce3157383c6b534b0bfd778741ddb
Stefan <stefan-horst@gmx.de>
2025-07-18 06:40:18 +0200

    add abstract method for applying action methods in elements

--- ptx/core.py
+++ ptx/core.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 from collections.abc import Callable
 
+from ptx.framework import PtxSystem
 
 class Element(ABC):
     """Abstract base class for all classes (commodities, components) of the PtX system."""
+    
+    @abstractmethod
+    def apply_action_method(self, method: Callable, ptx_system: PtxSystem, values: tuple) -> bool:
+        """Apply the values returned by a specific action method to the element and the ptx system."""
+        pass
     
     @abstractmethod
     def get_possible_observation_attributes(self, relevant_attributes: list[str]) -> list[str]:
