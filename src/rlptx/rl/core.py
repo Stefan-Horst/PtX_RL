e5c3e3e43a4da9ff467f3264e6e198456afc5af7
Stefan <stefan-horst@gmx.de>
2025-08-09 18:02:54 +0200

    add replay buffer

---
+++ src/rlptx/rl/core.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+
+class ReplayBuffer:
+    """Replay buffer for storing and sampling transitions from an environment. 
+    Every environment step produces transition data that is stored in the replay buffer. 
+    The replay buffer has a fixed capacity and overwrites the oldest transition if the buffer is full. 
+    Transition data consists of observations, next observations, rewards, actions, and terminateds."""
+    
+    def __init__(self, capacity, observations_shape, actions_shape, seed=None):
+        """Create a replay buffer with the given capacity of transitions that can be stored. 
+        A seed to control the random sampling can be specified."""
+        self.observations = np.empty((capacity, observations_shape), dtype=list)
+        self.next_observations = np.empty((capacity, observations_shape), dtype=list)
+        self.actions = np.empty((capacity, actions_shape), dtype=list)
+        self.rewards = np.empty((capacity, 1), dtype=np.float32)
+        self.terminateds = np.empty((capacity, 1), dtype=bool)
+        self.capacity = capacity
+        self.index = 0
+        self.full = False
+        self.rng = np.random.default_rng(seed)
+    
+    def add(self, observation, next_observation, reward, action, terminated):
+        """Add a new transition from an environment step to the replay buffer. 
+        If the buffer is full, the oldest transition is overwritten."""
+        self.observations[self.index] = observation
+        self.next_observations[self.index] = next_observation
+        self.rewards[self.index] = reward
+        self.actions[self.index] = action
+        self.terminateds[self.index] = terminated
+        self.index += 1
+        if self.index >= self.capacity:
+            self.index = 0
+            self.full = True
+    
+    def sample(self, batch_size=1):
+        """Sample a (batch of) transition(s) randomly from the replay buffer."""
+        length = self.capacity if self.full else self.index
+        indices = self.rng.integers(0, length, size=batch_size)
+        return (
+            torch.as_tensor(self.observations[indices]),
+            torch.as_tensor(self.next_observations[indices]),
+            torch.as_tensor(self.rewards[indices]),
+            torch.as_tensor(self.actions[indices]),
+            torch.as_tensor(self.terminateds[indices])
+        )
+    
