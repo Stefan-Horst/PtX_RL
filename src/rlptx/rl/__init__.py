482ff33eabc97231310e5c74027a9c3db2c65335
Stefan <stefan-horst@gmx.de>
2025-09-02 06:11:38 +0200

    add numpy elements to pytorch allowlist for model loading

--- src/rlptx/rl/__init__.py
+++ src/rlptx/rl/__init__.py
@@ -1,4 +1,14 @@
 import torch
+import torch.serialization
+import numpy
+
+
+# add these necessary numpy functionalities to the torch function allowlist used when loading 
+# models with torch.load(..., weights_only=True); relevant in rlptx.rl.core.load_sac_agent
+torch.serialization.add_safe_globals([
+    numpy.core.multiarray._reconstruct, numpy.ndarray, numpy.dtype, numpy.dtypes.Float32DType
+])
+
 
 # use gpu for network training in actor and critic if available
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
