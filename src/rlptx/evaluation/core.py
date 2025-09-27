a892d748b1f8c7c5db0a654670cc7ba28a57027b
Stefan <stefan-horst@gmx.de>
2025-09-27 06:23:29 +0200

    streamline log loading and plotting

--- src/rlptx/evaluation/core.py
+++ src/rlptx/evaluation/core.py
@@ -4,42 +4,49 @@ from rlptx.util import PROJECT_DIR
 from rlptx.logger import LOGFILE_PATH
 
 
-def load_agent_log(filename, path=LOGFILE_PATH):
-    """Load an agent log from a txt file and return it as a dataframe."""
-    assert "agent" in filename, "File is not an agent log."
+def load_log(filename, type="episode", path=LOGFILE_PATH):
+    """Load a log from a txt file and return it as a dataframe."""
+    assert type in ["episode", "agent", "test", "evaluation"], "File is not of a valid log type."
     filepath = PROJECT_DIR / (path + filename + ".txt")
-    agent_log = {}
-    with open(filepath, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.split(" - ")[-1].replace("\n", "")
-            elements = line.split(",")
-            for element in elements:
-                name, value = element.split(":", maxsplit=1)
-                name = name.strip()
-                if name in agent_log.keys():
-                    agent_log[name].append(value)
-                else:
-                    agent_log[name] = [value]
-    log_df = pd.DataFrame(agent_log, dtype=float)
-    return log_df
 
-def load_episode_log(filename, path=LOGFILE_PATH):
-    """Load an episode log from a txt file and return it as a dataframe."""
-    assert "episode" in filename, "File is not an episode log."
-    filepath = PROJECT_DIR / (path + filename + ".txt")
-    episode_log = {}
+    if type in ["episode", "test"]:
+        line_func = _line_episode_and_test
+    elif type == "agent":
+        line_func = _line_agent
+    elif type == "evaluation":
+        line_func = _line_evaluation
+    
+    # loop through the lines of the log file, get all key-value pairs put them into a dataframe
+    log = {}
     with open(filepath, "r") as f:
         lines = f.readlines()
         for line in lines:
-            if "reward" not in line:
-                continue # ignore log lines before first or after last episode or not including stats
-            elements = line.replace("\n", "").split(" - ")[4:]
+            elements = line_func(line)
             for element in elements:
                 name, value = element.split(": ", maxsplit=1)
-                if name in episode_log.keys():
-                    episode_log[name].append(value)
+                if name in log.keys():
+                    log[name].append(value)
                 else:
-                    episode_log[name] = [value]
-    log_df = pd.DataFrame(episode_log, dtype=float)
+                    log[name] = [value]
+    
+    log_df = pd.DataFrame(log, dtype=float)
     return log_df
+
+# Helper functions for parsing log lines of the different log types.
+# Each returns a list of elements in the form ["name1: value1", ...].
+
+def _line_episode_and_test(line):
+    # ignore log lines before first or after last episode or not including stats
+    if "reward" not in line:
+        return []
+    return line.replace("\n", "").split(" - ")[4:]
+
+def _line_agent(line):
+    return line.replace("\n", "").split(" - ")[-1].split(", ")
+
+def _line_evaluation(line):
+    episode, elements = line.replace("\n", "").split(" - ")[-2:]
+    episode = episode.split(" ")
+    episode = [f"{episode[0]}: {episode[1]}", f"{episode[2]}: {episode[3]}"]
+    episode.extend(elements.split(", "))
+    return episode
--- src/rlptx/evaluation/visualization.py
+++ src/rlptx/evaluation/visualization.py
@@ -1,33 +1,30 @@
-import os
-from pathlib import Path
 from matplotlib import pyplot as plt
 
-from rlptx.evaluation.core import load_episode_log, load_agent_log
+from rlptx.evaluation.core import load_log
 from rlptx.logger import LOGFILE_PATH
-from rlptx.util import get_most_recent_file, PROJECT_DIR
+from rlptx.util import get_most_recent_file
 
 
-def plot_episode_log(filename="", path=LOGFILE_PATH, plot_variables=[]):
+def plot_log(filename="", type="episode", path=LOGFILE_PATH, plot_variables=[]):
+    """Plot a log file for the given log of the given type or 
+    the most recent one of that type if no filename is given."""
     if filename == "": # use most recent file
-        filename = get_most_recent_file(path, search_string="episode")
-    log_df = load_episode_log(filename, path=path)
-    _plot_log(log_df, filename, plot_variables)
-
-def plot_agent_log(filename="", path=LOGFILE_PATH, plot_variables=[]):
-    if filename == "": # use most recent file
-        filename = get_most_recent_file(path, search_string="agent")
-    log_df = load_agent_log(filename, path=path)
-    _plot_log(log_df, filename, plot_variables)
-
-def _plot_log(log_df, name, plot_variables):
+        filename = get_most_recent_file(path, search_string=type)
+    log_df = load_log(filename, type=type, path=path)
+    
     if len(plot_variables) > 0:
         assert plot_variables in log_df.columns, "Log file does not contain all specified plot variables."
     else:
         plot_variables = log_df.columns
+    
     for variable in plot_variables:
-        fig, ax = plt.subplots()
-        ax.plot(log_df.index, log_df[variable])
-        ax.set(title=f"{variable} of {name}", xlabel="episode", ylabel=variable)
-        ax.grid(axis="y")
-        plt.show()
+        _plot_log(log_df, filename, variable)
+
+def _plot_log(log_df, name, variable):
+    """Plot a single variable of a log file."""
+    fig, ax = plt.subplots()
+    ax.plot(log_df.index, log_df[variable])
+    ax.set(title=f"{variable} of {name}", xlabel="episode", ylabel=variable)
+    ax.grid(axis="y")
+    plt.show()
     plt.close()
