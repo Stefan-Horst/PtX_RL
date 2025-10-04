c242fed5638f604fe1fbc03c077b162ef11c8182
Stefan <stefan-horst@gmx.de>
2025-10-04 06:59:39 +0200

    add option to only plot some episodes of evaluation log

--- src/rlptx/evaluation/visualization.py
+++ src/rlptx/evaluation/visualization.py
@@ -5,10 +5,12 @@ from rlptx.logger import LOGFILE_PATH
 from rlptx.util import get_most_recent_file
 
 
-def plot_log(filename="", type="episode", path=LOGFILE_PATH, plot_variables=[], cycle=1):
-    """Plot a log file for the given log of the given type or the most recent one of that type if no 
-    filename is given. If only some variables should be plotted, they can be specified. Cycle is 
-    only relevant for evaluation logs. It determines the test cycle to plot the logged variables for."""
+def plot_log(filename="", type="episode", path=LOGFILE_PATH, plot_variables=[], cycle=[1], episode=[]):
+    """Plot a log file for the given log of the given type or the most recent one of 
+    that type if no filename is given. If only some variables should be plotted, they 
+    can be specified. Cycle and episode are only relevant for evaluation logs. They 
+    determine the test cycles and their episodes to plot the logged variables for. 
+    Empty lists as values mean that all cycles or episodes will be plotted."""
     if filename == "": # use most recent file
         filename = get_most_recent_file(path, search_string=type)
     log_df = load_log(filename, type=type, path=path)
@@ -20,21 +22,25 @@ def plot_log(filename="", type="episode", path=LOGFILE_PATH, plot_variables=[], 
         plot_variables = plot_variables.drop(["Cycle", "Episode", "Step"], errors="ignore")
     
     if type == "evaluation":
-        # plot variables for each episode of the given testing cycle in separate plots
-        log_df = log_df[log_df["Cycle"] == cycle]
+        # plot variables for the given episodes of the given testing cycle in separate plots
+        log_df = log_df[log_df["Cycle"].isin(cycle)] if len(cycle) > 0 else log_df
+        log_df = log_df[log_df["Episode"].isin(episode)] if len(episode) > 0 else log_df
+        unique_cycles = log_df["Cycle"].unique()
         unique_episodes = log_df["Episode"].unique()
-        print(f"Creating {len(unique_episodes) * len(plot_variables)} plots for "
-              f"{len(unique_episodes)} episodes with {len(plot_variables)} variables...")
+        print(f"Creating {len(unique_cycles) * len(unique_episodes) * len(plot_variables)} "
+              f"plots for {len(unique_cycles)} cycle with each {len(unique_episodes)} "
+              f"episodes with {len(plot_variables)} variables...")
         empty_plots = 0
-        for episode in unique_episodes:
-            episode_df = log_df[log_df["Episode"] == episode]
-            if len(episode_df) <= 1:
-                empty_plots += 1
-                continue # don't plot episodes with just one step
-            for variable in plot_variables:
-                _plot_log(
-                    episode_df, f"{filename} - cycle {cycle} - episode {episode}", variable, x="Step"
-                )
+        for cycle in unique_cycles:
+            for episode in unique_episodes:
+                episode_df = log_df[log_df["Cycle"] == cycle][log_df["Episode"] == episode]
+                if len(episode_df) <= 1:
+                    empty_plots += 1
+                    continue # don't plot episodes with just one step
+                for variable in plot_variables:
+                    _plot_log(
+                        episode_df, f"{filename} - cycle {cycle} - episode {episode}", variable, x="Step"
+                    )
         if empty_plots > 0:
             print(f"Skipped plots for {empty_plots} episodes because they only contain one step.")
     else:
